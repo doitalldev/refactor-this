ab11ab95425ad168fe2a6338831b05806a5f71ba
Jonathan Clark <30505583+jonnyboydev@users.noreply.github.com>
2020-11-18 21:20:53 -0700

    {init} portfolio moving to this repo, portfolio repo will be archived when this one launches

---
+++ components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Card.scss";
+import Image from "next/image";
+
+export default function Card({ data }) {
+  return (
+    <section className="card">
+      <div className="card-data">
+        <h3 className="card-name">{data.name}</h3>
+        <span>Stack: {data.stack()}</span>
+        <div className="card-links">
+          {data.repo !== "" ? <a href={data.repo}>View Repo</a> : ""}
+          {data.livelink !== "" ? <a href={data.livelink}>View Live</a> : ""}
+        </div>
+        <p>{data.description}</p>
+        <p>{data.secondarydescription}</p>
+      </div>
+
+      <Image className="card-image" src={data.image} alt={data.name} unsized />
+    </section>
+  );
+}
---
+++ components/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Landing.scss";
+
+function Landing() {
+  return (
+    <div className="landing-info-text">
+      <h1>Welcome.</h1>
+      <h2>My name in Jonathan Clark.</h2>
+      <hr />
+    </div>
+  );
+}
+
+export default Landing;
